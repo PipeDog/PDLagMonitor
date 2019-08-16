49e65ac68b0092049973d3d7306659d80cf510cd
liang <18811130435@163.com>
2019-08-16 18:08:32 +0800

    Initial Commit

---
+++ PDLagMonitor/AppDelegate.h
@@ -0,0 +1,17 @@
+//
+//  AppDelegate.h
+//  PDLagMonitor
+//
+//  Created by 雷亮 on 2019/8/16.
+//  Copyright © 2019 liang. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface AppDelegate : UIResponder <UIApplicationDelegate>
+
+@property (strong, nonatomic) UIWindow *window;
+
+
+@end
+
---
+++ PDLagMonitor/ViewController.h
@@ -0,0 +1,15 @@
+//
+//  ViewController.h
+//  PDLagMonitor
+//
+//  Created by 雷亮 on 2019/8/16.
+//  Copyright © 2019 liang. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface ViewController : UIViewController
+
+
+@end
+
